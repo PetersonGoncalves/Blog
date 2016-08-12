57704b74cf42acc53d540bae6909f8fb64570a09
Peterson Gonçalves Bezerra Silva <peterson.gbs@hotmail.com>
2016-08-12 19:54:08 -0300

    Pacote Para testes

---
+++ src/Teste/TesteClasses.java
@@ -0,0 +1,25 @@
+package Teste;
+
+import Basicas.*;
+
+public class TesteClasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Teste 
+		Basica_Blog blog = new Basica_Blog();
+		
+		Basica_Usuario usuario = new Basica_Usuario();		
+		
+		Basica_Comentario comentario = new Basica_Comentario();
+		
+		Basica_Conteudo conteudo = new Basica_Conteudo();
+		
+		Basica_Notas notas = new Basica_Notas();
+		
+		
+
+	}
+
+}
